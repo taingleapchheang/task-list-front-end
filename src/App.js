e2f755b9b441b7c6af7fe1d78bd4199400a0220f
Jodi Denney <iarrived@gmail.com>
2022-06-21 13:59:09 -0700

    WAve 2 passed.  LIfted State to App and created delete task func

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TaskList from './components/TaskList.js';
 import './App.css';
 
@@ -16,13 +16,39 @@ const TASKS = [
 ];
 
 const App = () => {
+  const [tasks, setTasks] = useState(TASKS);
+
+  const setComplete = (id) => {
+    const newTasks = [];
+    for (const task of tasks) {
+      const newTask = { ...task };
+      if (newTask.id === id) {
+        newTask.isComplete = !newTask.isComplete;
+      }
+      newTasks.push(newTask);
+    }
+    setTasks(newTasks);
+  };
+
+  const deleteTask = (id) => {
+    const newTasks = tasks.filter((task) => task.id != id);
+    setTasks(newTasks);
+  };
   return (
     <div className="App">
       <header className="App-header">
         <h1>Ada&apos;s Task List</h1>
       </header>
       <main>
-        <div>{<TaskList tasks={TASKS} />}</div>
+        <div>
+          {
+            <TaskList
+              tasks={tasks}
+              setCompleteCallback={setComplete}
+              deleteTaskCallback={deleteTask}
+            />
+          }
+        </div>
       </main>
     </div>
   );
